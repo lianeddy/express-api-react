05e0252f9ee094bba25f28e33128556d9dc64e18
Firliandy <lian.eddy@gmail.com>
2020-04-24 11:54:06 +0700

    filter & sort socket

--- controller/socketController.js
+++ controller/socketController.js
@@ -2,23 +2,83 @@ const { db } = require('../database');
 
 module.exports = {
     getSocket : (req,res) => {
-        let sql = `select * from socket`;
+        let { orderBy, filterBy } = req.query;
+        let sql = `select * from socket s`;
+        if(filterBy){
+            sql += ` where s.read = ${filterBy}`
+        }
+        if(orderBy){
+            sql += ` order by s.read asc, id ${orderBy}`
+        }
+        console.log(sql)
         db.query(sql, (err, results) => {
             if(err)res.status(500).send(err.message)
+
+            let notifCount = results.filter((val) => val.read === 0).length
+            req.app.io.emit('notifCount', notifCount)
 
             res.status(200).send(results)
         })
     },
     addSocket : (req,res) => {
+        let { orderBy, filterBy } = req.query
         let sql = `insert into socket set ?`;
         db.query(sql, req.body, (err, insert) => {
+            if(err) res.status(500).send(err.message);
+            let sql = `select * from socket s`;
+            if(filterBy){
+                sql += ` where s.read = ${filterBy}`
+            }
+            if(orderBy){
+                sql += ` order by s.read asc, id ${orderBy}`
+            }
+            db.query(sql, (err,results) => {
+                if(err) res.status(500).send(err.message)
+                let notifCount = results.filter((val) => val.read === 0).length
+
+                req.app.io.emit('Socket', results);
+                req.app.io.emit('notifCount', notifCount)
+                // req.app.io.emit('Notif', results);
+            })
+        })
+    },
+    updateSocket : (req,res) => {
+        let { id } = req.params;
+        let { orderBy, filterBy } = req.query
+        console.log(req.query)
+        let sql = `update socket s set s.read = 1 where id = ${id}`;
+        db.query(sql,(err,update) => {
+            if(err) res.status(500).send(err.message);
+
+            let sql = `select * from socket s`;
+            if(filterBy){
+                sql += ` where s.read = ${filterBy}`
+            }
+            if(orderBy){
+                sql += ` order by s.read asc, id ${orderBy}`
+            }
+            db.query(sql, (err,results) => {
+                if(err) res.status(500).send(err.message)
+                let notifCount = results.filter((val) => val.read === 0).length
+
+                req.app.io.emit('Socket', results);
+                req.app.io.emit('notifCount', notifCount)
+                // req.app.io.emit('Notif', results);
+            })
+        })
+    },
+    updateAll : (req,res) => {
+        let sql =  `update socket s set s.read = 1`;
+        db.query(sql,(err,update) => {
             if(err) res.status(500).send(err.message);
 
             let sql = `select * from socket`;
             db.query(sql, (err,results) => {
                 if(err) res.status(500).send(err.message)
+                let notifCount = results.filter((val) => val.read === 0).length
 
                 req.app.io.emit('Socket', results);
+                req.app.io.emit('notifCount', notifCount)
                 // req.app.io.emit('Notif', results);
             })
         })
--- router/socketRouter.js
+++ router/socketRouter.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { 
     getSocket,
-    addSocket
+    addSocket,
+    updateSocket,
+    updateAll
 } = require('../controller').socketController;
 
 router.get('/get-socket', getSocket);
 router.post('/add-socket', addSocket);
+router.patch('/update-socket/:id', updateSocket);
+router.patch('/update-all', updateAll);
 
 module.exports = router;
